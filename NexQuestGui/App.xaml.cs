496d06309ee1f26d26fe4953b15372b3250217f6
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2025-05-25 22:48:47 +0100

    Added basic app host

--- NexQuestGui/App.xaml.cs
+++ NexQuestGui/App.xaml.cs
@@ -1,14 +1,31 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO;
 using System.Windows;
 
-namespace NexQuestGui
+namespace NexQuestGui;
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+    public static IHost? AppHost { get; private set; }
 
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        AppHost = Host.CreateDefaultBuilder()
+             .ConfigureAppConfiguration((context, config) =>
+             {
+                 var path = Directory.GetCurrentDirectory();
+                 config.SetBasePath(Directory.GetCurrentDirectory());
+                 config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+             })
+            .ConfigureServices((context, services) =>
+            {
+               
+            })
+            .Build();
+
+        AppHost.Start();
+
+        base.OnStartup(e);
+    }
 }
