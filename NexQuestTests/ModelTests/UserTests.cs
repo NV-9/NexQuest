10dc802f1e37a3f5334e3ec701e71e88b04ad436
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2025-05-25 22:41:27 +0100

    Added testing package

---
+++ NexQuestTests/ModelTests/UserTests.cs
@@ -0,0 +1,35 @@
+﻿using NexQuest.Models;
+
+namespace NexQuestTests.ModelTests;
+
+[TestClass]
+public sealed class UserTests
+{
+    [TestMethod]
+    public void GivenUserClassIsAvailableClassShouldNotThrow()
+    {
+        // Arrange
+        var user = new User();
+
+        // Act 
+
+        // Assert
+        Assert.IsNotNull(user);
+    }
+
+    [TestMethod]
+    public void GivenUserClassIsAvailableClassShouldBeConstructedWithVariableParams()
+    {
+        // Arrange
+        var userWithNoParams = new User();
+        var userWithUsername = new User("Test User");
+        var userWithPassword = new User("Test User", "Test Password");
+
+        // Act 
+        
+        // Assert
+        Assert.IsNotNull(userWithNoParams);
+        Assert.IsNotNull(userWithUsername);
+        Assert.IsNotNull(userWithPassword);
+    }
+}
