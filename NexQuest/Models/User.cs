ddda7e42d5b2ffa17896ec086e10e0dd3c0afb59
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2025-05-25 22:12:04 +0100

    Added entity framework and user model

---
+++ NexQuest/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace NexQuest.Models;
+public class User
+{
+    public User () { }
+
+    public User(string username)
+    {
+        UUID = Guid.NewGuid();
+        Username = username;
+    }
+
+    public User(string username, string password)
+    {
+        UUID = Guid.NewGuid();
+        Username = username;
+        Password = password;
+    }
+
+    public int ID { get; set; }
+    public Guid UUID { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+
+}
+public class UserConfiguration: IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(u => u.ID);
+
+        builder.Property(u => u.UUID).IsRequired();
+        builder.Property(u => u.Username).IsRequired();
+        builder.Property(u => u.Password).IsRequired();
+    }
+}
