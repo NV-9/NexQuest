c24d01e37fb154dbd87c3b8633c9e687855027c5
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2025-05-26 09:07:23 +0100

    Added migration and context factory

---
+++ NexQuest/Database/NexQuestDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace NexQuest.Database
+{
+    public class NexQuestDbContextFactory : IDesignTimeDbContextFactory<NexQuestDbContext>
+    {
+        public NexQuestDbContext CreateDbContext(string[] args)
+        {
+            var current = Directory.GetCurrentDirectory();
+            
+            var parent = Directory.GetParent(current)?.ToString();
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(parent)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<NexQuestDbContext>();
+            var connectionString = configuration.GetConnectionString("Default");
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new NexQuestDbContext(optionsBuilder.Options);
+        }
+    }
+}
