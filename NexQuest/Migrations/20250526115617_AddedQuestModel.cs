64d06d47225c241eb0ce8194c804b9aaf36c221b
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2025-05-26 12:56:54 +0100

    Added quest model

---
+++ NexQuest/Migrations/20250526115617_AddedQuestModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NexQuest.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedQuestModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Quest",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    ParentID = table.Column<int>(type: "int", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quest", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Quest_Quest_ParentID",
+                        column: x => x.ParentID,
+                        principalTable: "Quest",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quest_ParentID",
+                table: "Quest",
+                column: "ParentID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quest");
+        }
+    }
+}
---
+++ NexQuest/Models/Quest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace NexQuest.Models;
+public class Quest
+{
+    public Quest() { }
+
+    public Quest(string title, string description, QuestType type = QuestType.MainQuest, Quest? parent = null) 
+    {
+        Title = title;
+        Description = description;
+        Type = type;
+        Parent = parent;
+        CreatedAt = DateTime.Now;
+        UpdatedAt = DateTime.Now;
+    }
+
+    public int ID { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public QuestType Type { get; set; }
+    public Quest? Parent { get; set; }
+    public Quest[] Children { get; set; } = [];
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+
+}
+public class QuestConfiguration : IEntityTypeConfiguration<Quest>
+{
+    public void Configure(EntityTypeBuilder<Quest> builder)
+    {
+        builder.HasKey(u => u.ID);
+
+        builder.Property(u => u.Title).IsRequired();
+        builder.Property(u => u.Description).IsRequired();
+        builder.Property(u => u.Type).IsRequired();
+        builder.Property(u => u.CreatedAt).IsRequired();
+        builder.Property(u => u.UpdatedAt).IsRequired();
+    }
+}
+
+public enum QuestType
+{
+    MainQuest,
+    SideQuest,
+    SubQuest,
+}
